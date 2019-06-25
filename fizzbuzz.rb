25724ed32ef28c9ca623b1a0691aead11f72c6ad
ahetaro <bisk1280392@gn.iwasaki.ac.jp>
2019-06-25 16:41:55 +0900

    fizzbuzz修正

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
-    if num % 15 == 0
+    if num % 15 === 0
         puts "fizzbuzz"
-    elsif num % 3 == 0
+    elsif num % 3 === 0
         puts "fizz"
-    elsif num % 5 ==0
+    elsif num % 5 ===0
         puts "buzz"
     else 
         puts num.to_s
@@ -11,11 +11,11 @@ def fizzbuzz(num)
 end
 
     puts "数字を入力してください"
-    input = gets
-    while input.kind_of?(String)
-        puts "ちゃんと数字を入れてください"
+    num = gets.to_i
+    while num === 0 do
         puts "数字を入力してください"
-        input = gets
+        num = gets.to_i
     end
+    
     puts "結果は..."
-    puts fizzbuzz(input)
+    puts fizzbuzz(num)
